e4e2d6bd897f25da0b9b38e9f96c515b291f16eb
Nathaniel <nathaniel@example.com>
2026-09-11 17:02:43 +0800

    Add two more values (5 and 45) to test stack sorting

--- Stacks/App.java
+++ Stacks/App.java
@@ -12,6 +12,8 @@ public class App {
         theStack.push(10);
         theStack.push(30);
         theStack.push(50);
+        theStack.push(5);
+        theStack.push(45);
 
         System.out.println("=== Stack Before Sorting ===");
         printStack(theStack);

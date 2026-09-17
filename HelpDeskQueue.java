c8cec2d56dbe163064a64cf72e5a7b73b6799c6f
nathaniel_sarcia <edgarnathanielsarcia@gmail.com>
2026-09-17 22:21:20 +0800

    Add files via upload

---
+++ HelpDeskQueue.java
@@ -0,0 +1,8 @@
+public class HelpDeskQueue {
+    
+    public static void main(String[] args) {
+        // Help Desk Queue - MP1 DSA
+        // TODO: Implement queue-based help desk system
+    }
+    
+}
---
+++ UndoManager.java
@@ -0,0 +1,8 @@
+public class UndoManager {
+    
+    public static void main(String[] args) {
+        // Undo Manager - MP1 DSA
+        // TODO: Implement undo/redo functionality (likely using a stack)
+    }
+    
+}
